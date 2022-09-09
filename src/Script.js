3d48ec7efde4c3f96ff7e144cdb45df94d730e6c
Alejandro Morales <lamv.project@gmail.com>
2022-09-09 10:18:23 -0600

    Finishing the logic of the project

--- src/Script.js
+++ src/Script.js
@@ -1,14 +1,13 @@
 /**
  * DOM OBJECTS
  */
-const $boardGame = document.querySelector('.board-container')
+const $boardGame = document.querySelector('.board')
 const $statusGame = document.querySelector('.status-game')
 const $restartButton = document.querySelector('.btn-restart')
 
-/* Array creado para verificaciones con el tablero*/
-const GAME_STATE = ['', '', '', '', '', '', '', '', '']
-/* Array creado para posibles combinaciones de victoria*/
-/* eslint-disable-next-line no-unused-vars */
+/* Array creado para verificaciones con el tablero */
+const GAME_STATE = [undefined || '', '', '', '', '', '', '', '', '']
+/* Array creado para posibles combinaciones de victoria */
 const WINNING_POS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -29,16 +28,13 @@ const RANDOM_NUMBER = () => Math.floor(Math.random() * 100)
  * This Two function returns messages for the game
  */
 
-/* eslint-disable-next-line no-unused-vars */
 const WINNER_MESSAGE = () => `The winner is ${currentPlayer}`
-/* eslint-disable-next-line no-unused-vars */
 const DRAW_MESSAGE = () => '---): Draw :(---'
 const TURN_MESSAGE = () => `Turn of ${currentPlayer}`
 
 // Variable used to identify our token (PLAYER)
-/* eslint-disable-next-line */
-let playing = true
 let currentPlayer = RANDOM_NUMBER() > 50 ? 'X' : 'O'
+let playing = true
 
 /**
  * This is the main FUNCTION
@@ -50,8 +46,8 @@ function main() {
 
 main()
 
-function handleStatusDisplay(turn) {
-  $statusGame.innerText = turn
+function handleStatusDisplay(message) {
+  $statusGame.innerText = message
 }
 
 function listeners() {
@@ -67,44 +63,41 @@ function handleCellClick(e) {
       $clickedCell
     )
 
-    if (GAME_STATE[clickedCellIndex] === '') {
-      validationMove($clickedCell, clickedCellIndex)
-      handleMovePlayer()
-      handleTurnPlayer()
+    if (GAME_STATE[clickedCellIndex] === '' && playing) {
+      handleMovePlayer($clickedCell, clickedCellIndex)
+      validationMove()
+    } else if (!playing) {
+      /* Hacer un modal que muestre juego termino y presentar el boton restart */
+      console.log('Game Over, Restart to play again')
     } else {
       /* Hacer un modal que muestre el erro al presionar casilla ocupada */
-      console.log('busy')
+      console.log('Busy')
     }
   }
 }
 
-function validationMove($cell, index) {
+function handleMovePlayer($cell, index) {
   GAME_STATE[index] = currentPlayer
   $cell.innerText = currentPlayer
 }
 
-function handleMovePlayer() {
+function validationMove() {
   let winner = false
 
-  for (let i = 0; i < GAME_STATE.length; i++) {
-
+  for (let i = 0; i < WINNING_POS.length; i++) {
     let posOne = GAME_STATE[WINNING_POS[i][0]]
     let posTwo = GAME_STATE[WINNING_POS[i][1]]
     let posThree = GAME_STATE[WINNING_POS[i][2]]
 
-    if (posOne === '' || posTwo === '' || posThree === '') {
-      console.log('position empty')
-      continue
-    }
+    if (posOne === '' || posTwo === '' || posThree === '') continue
 
     if (posOne === posTwo && posTwo === posThree) {
-      console.log('Winner')
       winner = !winner
       break
     }
   }
 
-  if(winner){
+  if (winner) {
     handleStatusDisplay(WINNER_MESSAGE())
     playing = !playing
     return
@@ -112,19 +105,33 @@ function handleMovePlayer() {
 
   let draw = GAME_STATE.includes('')
 
-  if(!draw){
+  if (!draw && playing) {
     handleStatusDisplay(DRAW_MESSAGE())
     playing = !playing
+    return
   }
-  
+
+  handleTurnPlayer()
 }
 
-/* eslint-disable-next-line */
 function handleTurnPlayer() {
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
   handleStatusDisplay(TURN_MESSAGE())
 }
 
 function handleRestartClick() {
-  console.log('Click on restart button')
+  cleanBoard()
+  currentPlayer = RANDOM_NUMBER() > 50? 'X' : 'O'
+  playing = true
+  handleStatusDisplay(TURN_MESSAGE())
+}
+
+function cleanBoard() {
+  let i = 0
+
+  while (i < GAME_STATE.length) {
+    GAME_STATE[i] = ''
+    $boardGame.children[i].innerText = ''
+    i++
+  }
 }
